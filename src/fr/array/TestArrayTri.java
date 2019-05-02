460fdd5b74f0b578f808596435aa23e3789c4083
poloch0n <50014021+poloch0n@users.noreply.github.com>
2019-05-02 16:01:32 +0200

    exercice 3

---
+++ src/fr/array/TestArrayTri.java
@@ -0,0 +1,15 @@
+package fr.array;
+
+import java.util.*;
+
+public class TestArrayTri {
+
+	public static void main(String [] args) {
+		int [] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        Arrays.sort(array);
+		for (int element : array) {
+			System.out.print( element + " ");
+		}
+	}
+}
